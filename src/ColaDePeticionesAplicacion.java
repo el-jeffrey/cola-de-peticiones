3fdafb290d05f9f114f4bcd4a60562f2dca37b60
jespinel <jespinelp.d@outlook.com>
2020-05-26 09:20:50 -0500

    Avances diseño pantalla principal

--- src/ColaDePeticionesAplicacion.java
+++ src/ColaDePeticionesAplicacion.java
@@ -7,10 +7,19 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JSplitPane;
 import javax.swing.JLabel;
 import javax.swing.JButton;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.ImageIcon;
+import javax.swing.JTable;
+import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
+import javax.swing.table.DefaultTableModel;
 
 public class ColaDePeticionesAplicacion extends JFrame {
 
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -20,6 +29,7 @@ public class ColaDePeticionesAplicacion extends JFrame {
 			public void run() {
 				try {
 					ColaDePeticionesAplicacion frame = new ColaDePeticionesAplicacion();
+					frame.setLocationRelativeTo(null);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -34,22 +44,71 @@ public class ColaDePeticionesAplicacion extends JFrame {
 	public ColaDePeticionesAplicacion() {
 		setTitle("Cola de Peticiones HTTP");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 760, 493);
+		setBounds(100, 100, 960, 493);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
 		
-		JSplitPane splitPane = new JSplitPane();
-		splitPane.setBorder(null);
-		contentPane.add(splitPane, BorderLayout.NORTH);
-		
 		JLabel lblNewLabel = new JLabel("Ingrese los request a la Cola de Peticiones HTTP");
-		splitPane.setLeftComponent(lblNewLabel);
 		
-		JButton btnNewButton = new JButton("Nuevo");
-		splitPane.setRightComponent(btnNewButton);
+		JButton btnNuevo = new JButton("Nuevo");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+
+		
+
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 714, Short.MAX_VALUE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblNewLabel)
+							.addPreferredGap(ComponentPlacement.RELATED, 368, Short.MAX_VALUE)
+							.addComponent(btnNuevo, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(btnNuevo))
+					.addGap(18)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 341, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(51, Short.MAX_VALUE))
+		);
+		
+	
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{1,"GET", "Crear Usuario"}
+			},
+			new String[] {
+				"#", "Método", "Título", "URL", "Body", "Autenticación", "Headers", ""
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				true, true, true, true, true, true, false, true
+			};
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		});
+		table.getColumnModel().getColumn(0).setPreferredWidth(30);
+		table.getColumnModel().getColumn(0).setMaxWidth(35);
+		table.getColumnModel().getColumn(1).setPreferredWidth(50);
+		table.getColumnModel().getColumn(2).setPreferredWidth(250);
+		scrollPane.setViewportView(table);
+		contentPane.setLayout(gl_contentPane);
 
 	}
-
+	
+	
 }
