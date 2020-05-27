4976c7804c6355dabd93ca278f8731468799b198
jespinel <jespinelp.d@outlook.com>
2020-05-27 08:28:11 -0500

    Avances modal

--- src/ColaDePeticionesAplicacion.java
+++ src/ColaDePeticionesAplicacion.java
@@ -1,12 +1,20 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JSplitPane;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -16,10 +24,13 @@ import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.table.DefaultTableModel;
 
-public class ColaDePeticionesAplicacion extends JFrame {
+public class ColaDePeticionesAplicacion extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
 	private JTable table;
+	private static JDialog d;
+	static JFrame f;
+	String tituloCapturaPeticion = "Nueva Petición";
 
 	/**
 	 * Launch the application.
@@ -42,61 +53,44 @@ public class ColaDePeticionesAplicacion extends JFrame {
 	 * Create the frame.
 	 */
 	public ColaDePeticionesAplicacion() {
+
 		setTitle("Cola de Peticiones HTTP");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 960, 493);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
-		JLabel lblNewLabel = new JLabel("Ingrese los request a la Cola de Peticiones HTTP");
-		
-		JButton btnNuevo = new JButton("Nuevo");
-		
-		JScrollPane scrollPane = new JScrollPane();
-		
 
-		
+		JLabel lblNewLabel = new JLabel("Ingrese los request a la Cola de Peticiones HTTP");
+
+		JButton btnNuevo = new JButton("Nuevo");
+		btnNuevo.setActionCommand("get-request");
+		btnNuevo.addActionListener(this);
+
+		JScrollPane scrollPane = new JScrollPane();
 
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
-		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+		gl_contentPane.setHorizontalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING).addGroup(gl_contentPane
+				.createSequentialGroup().addContainerGap()
+				.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addComponent(scrollPane, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 714, Short.MAX_VALUE)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(lblNewLabel)
-							.addPreferredGap(ComponentPlacement.RELATED, 368, Short.MAX_VALUE)
-							.addComponent(btnNuevo, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap())
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
-						.addComponent(lblNewLabel)
-						.addComponent(btnNuevo))
-					.addGap(18)
-					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 341, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(51, Short.MAX_VALUE))
-		);
-		
-	
-		
+						.addGroup(gl_contentPane.createSequentialGroup().addComponent(lblNewLabel)
+								.addPreferredGap(ComponentPlacement.RELATED, 368, Short.MAX_VALUE)
+								.addComponent(btnNuevo, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)))
+				.addContainerGap()));
+		gl_contentPane.setVerticalGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup().addContainerGap()
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE).addComponent(lblNewLabel)
+								.addComponent(btnNuevo))
+						.addGap(18)
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 341, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(51, Short.MAX_VALUE)));
+
 		table = new JTable();
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{1,"GET", "Crear Usuario"}
-			},
-			new String[] {
-				"#", "Método", "Título", "URL", "Body", "Autenticación", "Headers", ""
-			}
-		) {
-			boolean[] columnEditables = new boolean[] {
-				true, true, true, true, true, true, false, true
-			};
+		table.setModel(new DefaultTableModel(new Object[][] { { 1, "GET", "Crear Usuario" } },
+				new String[] { "#", "Método", "Título", "URL", "Body", "Autenticación", "Headers", "" }) {
+			boolean[] columnEditables = new boolean[] { true, true, true, true, true, true, false, true };
+
 			public boolean isCellEditable(int row, int column) {
 				return false;
 			}
@@ -109,6 +103,19 @@ public class ColaDePeticionesAplicacion extends JFrame {
 		contentPane.setLayout(gl_contentPane);
 
 	}
-	
-	
+
+	public void actionPerformed(ActionEvent ae) {
+		String choice = ae.getActionCommand();
+		if (choice.equals("Quit")) {
+			System.exit(0);
+		} else if (choice.equals("get-request")) {
+//			CapturarPeticion dlg = new CapturarPeticion(this, tituloCapturaPeticion);
+			GetPeticion  dlg = new GetPeticion();
+			String results = dlg.run();
+			/*if (results[0] != null) {
+				JOptionPane.showMessageDialog(this, results[0] + ", color: " + results[1]);
+			}*/
+		}
+	}
+
 }
---
+++ src/GetPeticion.java
@@ -0,0 +1,149 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.border.EmptyBorder;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+
+public class GetPeticion extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	public GetPeticion() {
+		setTitle("Nueva Petici\u00F3n");
+
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 700, 333);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		
+		JLabel lblNewLabel = new JLabel("T\u00EDtulo:");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("M\u00E9todo:");
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"GET", "POST"}));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		
+		JLabel lblUrl = new JLabel("URL:");
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Guardar");
+		GroupLayout gl_contentPane = new GroupLayout(contentPane);
+		gl_contentPane.setHorizontalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblNewLabel)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textField, GroupLayout.PREFERRED_SIZE, 442, GroupLayout.PREFERRED_SIZE)
+							.addGap(18)
+							.addComponent(lblNewLabel_1)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(comboBox, 0, 116, Short.MAX_VALUE))
+						.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 656, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(lblUrl, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
+							.addPreferredGap(ComponentPlacement.RELATED)
+							.addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 620, Short.MAX_VALUE))
+						.addComponent(btnNewButton, Alignment.TRAILING))
+					.addContainerGap())
+		);
+		gl_contentPane.setVerticalGroup(
+			gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblNewLabel_1)
+						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(lblUrl))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(scrollPane, GroupLayout.PREFERRED_SIZE, 180, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(btnNewButton)
+					.addGap(16))
+		);
+		
+		JTabbedPane pestañas = new JTabbedPane(JTabbedPane.TOP);
+		
+		JPanel panelBody = new JPanel();		
+		pestañas.add("Body", panelBody);
+		
+		JTextArea textArea = new JTextArea();
+		GroupLayout gl_panelBody = new GroupLayout(panelBody);
+		gl_panelBody.setHorizontalGroup(
+			gl_panelBody.createParallelGroup(Alignment.LEADING)
+				.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 649, Short.MAX_VALUE)
+		);
+		gl_panelBody.setVerticalGroup(
+			gl_panelBody.createParallelGroup(Alignment.LEADING)
+				.addComponent(textArea, GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE)
+		);
+		panelBody.setLayout(gl_panelBody);
+		
+		JPanel panelAut = new JPanel();		
+		pestañas.add("Autenticación", panelAut);
+		
+		JPanel panelHeaders = new JPanel();		
+		pestañas.add("Headers", panelHeaders);
+		
+		scrollPane.setViewportView(pestañas);
+		contentPane.setLayout(gl_contentPane);
+	      createAndShowUI();
+	  }
+
+	  private void createAndShowUI(){
+	    /*setTitle("This JFRAME looks like JDialog");
+	    setSize(new Dimension(500,100));
+	    setUndecorated(true);
+	    setResizable(false);
+	    getRootPane().setWindowDecorationStyle(JRootPane.PLAIN_DIALOG);
+	    setLocationRelativeTo(null);
+	    setDefaultCloseOperation(EXIT_ON_CLOSE);*/
+	    //addWindowListener(new WindowAdapter(){
+	    //  public void windowClosing(WindowEvent e) {
+	    //    System.out.println("Window Closing");
+	    //    System.exit(0);
+	    //  }
+	    //});
+	  }
+	
+	/**
+	 * Launch the application.
+	 */
+	public static String run() {
+		GetPeticion frame = new GetPeticion();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		return "ok";
+	}
+}
